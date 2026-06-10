f587aa7e4a68d4ab5df9b25c567e84aa73620395
Lucas <contato.lucasguimaraes1@gmail.com>
2026-06-10 13:53:49 -0300

    Create the first version of Class SistemaEventos

--- src/sistema/SistemaEventos.java
+++ src/sistema/SistemaEventos.java
@@ -1,5 +1,27 @@
 package sistema;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import modelo.Atividade;
+import modelo.Palestrante;
+import modelo.Participante;
+
 public class SistemaEventos {
-    
+    private Map<String, Participante> participantesPorMatricula = new HashMap<>();
+    private Map<String, Palestrante> palestrantesPorCpf = new HashMap<>();
+    private List<Atividade> atividades = new ArrayList<>();
+    private String[] diasFuncionamento;
+
+    public SistemaEventos() {
+        diasFuncionamento = new String[] {
+            "Segunda",
+            "Terça",
+            "Quarta",
+            "Quinta",
+            "Sexta"
+        };
+    }
 }
